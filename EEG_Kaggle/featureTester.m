6df065dab8a7d20e9ed7019a2cb544a66dad83ef
Bishal Santra <bs.bytestorm@gmail.com>
2016-11-10 13:46:12 +0530

    fdslkj

--- EEG_Kaggle/featureTester.m
+++ EEG_Kaggle/featureTester.m
@@ -46,24 +46,63 @@ y0 = f0.dataStruct.data(:,1);
 y1 = f1.dataStruct.data(:,1);
 
 %% Histogram
-
-left = 0;
-right = 0;
-k = 1.5;
-edges = [0];
-for i = 1:15
-    left = left - i^k;
-    right = right + i^k;
-    edges = [left edges right];
-end
-
 figure(1)
 clf
 hold on
 histogram(y1, edges)
 histogram(y0, edges)
 hold off
-legend('Preictal', 'Interictal')
+
+%% Get feature matrix with label column attached
+
+left = 0;
+right = 0;
+k = 1.3;
+edges = [0];
+for i = 1:20
+    left = left - i^k;
+    right = right + i^k;
+    edges = [left edges right];
+end
+
+for user = 1:3
+    [~, fselect0] = getSafeList(user, 0);
+    [~, fselect1] = getSafeList(user, 1);
+    fd = length(edges);
+    feat3DMat = zeros(length(fselect0) + length(fselect1),...
+         fd*16 + 1);
+    h = waitbar(0, '', 'Name', 'Extracting Juice...');
+    steps = length(fselect0) + length(fselect1);
+    for i = 1:length(fselect0)
+        waitbar(i / steps, h, sprintf('Progress %0.2f%%', 100*i / steps));
+        f = fselect0{i};
+        f = load(f);
+        for c = 1:16
+            feat3DMat(i, ((c-1)*fd + 1):(c*fd) ) = histc(f.dataStruct.data(:, c), edges);
+        end
+        feat3DMat(i, end) = 0;
+    end
+    offset = length(fselect0);
+    for i = 1:length(fselect1)
+        waitbar((offset + i) / steps, h, sprintf('Progress %0.2f%%', 100*(offset + i) / steps));
+        f = fselect1{i};
+        f = load(f);
+        for c = 1:16
+            feat3DMat(offset + i, ((c-1)*fd + 1):(c*fd) ) = histc(f.dataStruct.data(:, c), edges);
+        end
+        feat3DMat(offset + i, end) = 1;
+    end
+    close(h);
+    save(sprintf('feat3DMat_%d', user), 'feat3DMat', '-v7.3');
+end
+
+%%
+rng(1)
+
+SVMModel = fitcsvm(feat3DMat(:, 1:end-1),feat3DMat(:, end),'Standardize',true,'KernelFunction','Polynomial', 'PolynomialOrder', 2, ...
+    'KernelScale','auto');
+CVSVMModel = crossval(SVMModel);
+classLoss = kfoldLoss(CVSVMModel)
 
 %%
 figure(3)
